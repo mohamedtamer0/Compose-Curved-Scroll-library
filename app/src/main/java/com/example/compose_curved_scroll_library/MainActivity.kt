7181197972fe1db12b56177bd044a7b018be4e7c
mohamedtamer0 <mt4452242@gmail.com>
2022-01-29 20:46:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/compose_curved_scroll_library/MainActivity.kt
@@ -0,0 +1,127 @@
+package com.example.compose_curved_scroll_library
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.compose_curved_scroll.CurvedScrollView
+import com.example.compose_curved_scroll_library.ui.theme.ComposeCurvedScrolllibraryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCurvedScrolllibraryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    CurvedScroll()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CurvedScroll() {
+    //List of item add
+    val items = listOf(
+        "Dog1",
+        "Dog2",
+        "Dog3",
+        "Dog4",
+        "Dog5",
+        "Dog6",
+        "Dog7",
+        "Dog8",
+        "Dog9"
+    )
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(55.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Curved Scrollview",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 10.dp, end = 10.dp),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            CurvedScrollView(items.size) { index ->
+                Column(
+                    modifier = Modifier.wrapContentSize()
+                ) {
+                    Image(
+                        painter = painterResource(
+                            id =
+                            when (index) {
+                                0 -> R.drawable.a
+                                1 -> R.drawable.b
+                                2 -> R.drawable.c
+                                3 -> R.drawable.d
+                                4 -> R.drawable.e
+                                5 -> R.drawable.f
+                                6 -> R.drawable.g
+                                7 -> R.drawable.h
+                                8 -> R.drawable.i
+                                else -> R.drawable.j
+                            }
+                        ),
+                        contentDescription = "Curved Image",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .size(70.dp)
+                            .clip(CircleShape)
+                    )
+
+                    Spacer(modifier = Modifier.padding(5.dp))
+
+                    Text(
+                        text = items[index],
+                        style = MaterialTheme.typography.h6
+                    )
+                }
+            }
+
+            Image(
+                painter = painterResource(id = R.drawable.a),
+                contentDescription = "Curved Logo Image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .width(120.dp)
+                    .height(150.dp)
+                    .clip(RoundedCornerShape(20.dp))
+            )
+        }
+    }
+}
---
+++ compose-curved-scroll/src/main/java/com/example/compose_curved_scroll/CurvedScrollView.kt
@@ -0,0 +1,93 @@
+package com.example.compose_curved_scroll
+
+import androidx.compose.foundation.gestures.FlingBehavior
+import androidx.compose.foundation.gestures.ScrollScope
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.cos
+
+@Composable
+fun CurvedScrollView(
+    itemCount: Int,
+    item: @Composable (Int) -> Unit
+) {
+    val scrollState = rememberScrollState()
+    var size = remember { mutableStateOf(IntSize.Zero) }
+    val scope = rememberCoroutineScope()
+    val indices = remember { IntArray(itemCount) { 0 } }
+
+    val flingBehaviour = object : FlingBehavior {
+        override suspend fun ScrollScope.performFling(initialVelocity: Float): Float {
+            val value = scrollState.value
+            indices.minByOrNull { abs(it - value) }?.let {
+                scope.launch {
+                    scrollState.animateScrollTo(it)
+                }
+            }
+            return initialVelocity
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .onSizeChanged {
+                size.value = it
+            }
+    ) {
+        Layout(
+            content = {
+                repeat(itemCount) {
+                    item(it)
+                }
+            },
+            modifier = Modifier.verticalScroll(
+                scrollState, flingBehavior = flingBehaviour
+            )
+        ) { measurables, constraints ->
+            val itemSpacing = 16.dp.roundToPx()
+            var contentHeight = (itemCount - 1) * itemSpacing
+
+            val placeables = measurables.mapIndexed { index, measurable ->
+                val placeable = measurable.measure(constraints = constraints)
+                contentHeight += if (index == 0 || index == measurables.lastIndex) {
+                    placeable.height / 2
+                } else {
+                    placeable.height
+                }
+                placeable
+            }
+
+            layout(constraints.maxWidth, size.value.height + contentHeight) {
+                val startOffset = size.value.height / 2 - placeables[0].height / 2
+                var yPosition = startOffset
+
+                val scrollPercent = scrollState.value.toFloat() / scrollState.maxValue
+
+                placeables.forEachIndexed { index, placeable ->
+                    val elementRatio = index.toFloat() / placeables.lastIndex
+                    val interpolatedValue = cos((scrollPercent - elementRatio) * PI)
+                    val indent = interpolatedValue * size.value.width / 2
+
+                    placeable.placeRelativeWithLayer(x = indent.toInt(), y = yPosition) {
+                        alpha = interpolatedValue.toFloat()
+                    }
+                    indices[index] = yPosition - startOffset
+                    yPosition += placeable.height + itemSpacing
+                }
+            }
+        }
+    }
+}
